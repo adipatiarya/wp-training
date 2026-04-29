cc4cc262c2773d72c9cff24cd7744c3d1b539298
RatmanDev <suratman@hype-plus.com>
2026-04-29 22:45:00 +0700

    ## Bagian 7: Events Post Type

--- wp-content/themes/fictional-univeristy-theme/page-past-events.php
+++ wp-content/themes/fictional-univeristy-theme/page-past-events.php
@@ -5,17 +5,33 @@ get_header(); ?>
 <div class="page-banner">
   <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg') ?>);"></div>
   <div class="page-banner__content container container--narrow">
-    <h1 class="page-banner__title">All Events</h1>
+    <h1 class="page-banner__title">Past Events</h1>
     <div class="page-banner__intro">
-      <p>See what is going on in our world.</p>
+      <p>A recap of our past events.</p>
     </div>
   </div>  
 </div>
 
 <div class="container container--narrow page-section">
 <?php
-  while(have_posts()) {
-    the_post(); ?>
+
+  $pastEvents = new WP_Query([
+    'paged' => get_query_var('paged', 1),
+    'post_type' => 'event',
+    'meta_key' => 'event_date',
+    'orderby' => 'meta_value_num',
+    'order' => 'DESC',
+    'meta_query' => [
+        [
+            'key' => 'event_date',
+            'compare' => '<',
+            'value' => date('Ymd'),
+            'type' => 'numeric'
+        ]
+    ]
+  ]);
+  while($pastEvents->have_posts()) {
+    $pastEvents->the_post(); ?>
     <div class="event-summary">
       <a class="event-summary__date t-center" href="#">
           <?php 
@@ -30,9 +46,10 @@ get_header(); ?>
       </div>
     </div>
   <?php }
-  echo paginate_links();
+  echo paginate_links(array(
+    'total' => $pastEvents->max_num_pages
+  ));
 ?>
-<p>Looking for past events? <a href="<?php echo site_url('/past-events'); ?>">View them here</a>.</p>
 </div>
 
 <?php get_footer();
